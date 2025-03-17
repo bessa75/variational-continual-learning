04bce1046985eb2bcb4521b492e59abf9337f64d
bessa75 <bessaswann@gmail.com>
2025-03-17 16:37:45 +0100

    Ajout de nouveaux fichiers

--- ddm/run_permuted.py
+++ ddm/run_permuted.py
@@ -62,7 +62,7 @@ coreset_size = 0
 data_gen = PermutedMnistGenerator(num_tasks)
 vcl_result = vcl.run_vcl(hidden_size, no_epochs, data_gen, 
     coreset.rand_from_batch, coreset_size, batch_size, single_head)
-print vcl_result
+print(vcl_result)
 
 # Run random coreset VCL
 tf.reset_default_graph()
@@ -73,7 +73,7 @@ coreset_size = 200
 data_gen = PermutedMnistGenerator(num_tasks)
 rand_vcl_result = vcl.run_vcl(hidden_size, no_epochs, data_gen, 
     coreset.rand_from_batch, coreset_size, batch_size, single_head)
-print rand_vcl_result
+print(rand_vcl_result)
 
 # Run k-center coreset VCL
 tf.reset_default_graph()
@@ -83,7 +83,7 @@ np.random.seed(1)
 data_gen = PermutedMnistGenerator(num_tasks)
 kcen_vcl_result = vcl.run_vcl(hidden_size, no_epochs, data_gen, 
     coreset.k_center, coreset_size, batch_size, single_head)
-print kcen_vcl_result
+print(kcen_vcl_result)
 
 # Plot average accuracy
 vcl_avg = np.nanmean(vcl_result, 1)
